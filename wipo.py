185f5c679e7716fbcd6ffa60461e90f811485cf1
pramod9349 <98535879+pramod9349@users.noreply.github.com>
2022-02-03 14:28:26 +0530

    Last used

--- wipo.py
+++ wipo.py
@@ -5,14 +5,18 @@ headers = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 status_url= f'https://patentscope.wipo.int/search/en/detail.jsf?docId=WO2020191111&_cid=P11-KQ6CCK-39257-2'
 html = session.get(status_url)
 html = html.html
-html = session.get(status_url)
-print(html)
-html = html.html
-print("this is reaspnse html")
-print(html)
+publicationNumber = ''
+elements = html.find('div.ps-field.ps-biblio-field')
+for element in elements:
+    temp = element.text
+    if temp.find('Publication Number') != -1:
+        temp = temp.split()
+        temp.reverse()
+        publicationNumber = temp[0]
 
-thisval = html.find('div.ps-biblio-data',first= True)
-print(thisval.text)
-print(thisval)
-publication= thisval[0].find('#detailMainForm:MyTabViewId:j_idt1683:j_idt1688').text
-print(publication)
+print(publicationNumber)
+#thisval = html.find('div.ps-biblio-data',first= True)
+#print(thisval.text.find('Publication'))
+#print(thisval)
+#publication= thisval[0].find('#detailMainForm:MyTabViewId:j_idt1683:j_idt1688').text
+#print(publication)
